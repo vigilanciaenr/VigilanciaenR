b26160b0397027954c3bdf60be752b3b76674599
vigilancia en r <vigilanciaenr@gmail.com>
2024-10-09 10:52:47 -0300

    Initial commit

--- test.R
+++
@@ -1,2 +0,0 @@
-usethis::use_git_remote("origin", url = "https://github.com/vigilanciaenr/VigilanciaenR.git")
-usethis::use_git_remote("origin", url = "https://github.com/vigilanciaenr/VigilanciaenR.git", overwrite = TRUE)
